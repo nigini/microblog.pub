ff720294e54fbbf827be99347690f772c36c1ad3
1dom <>
2024-09-22 18:06:17 +0100

    Add docs, tests and fixes for custom html route.

---
+++ tests/test_customization.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+from unittest.mock import MagicMock
+
+from app.main import app
+from app.customization import get_custom_router, register_html_page, _CUSTOM_ROUTES
+
+
+
+def test_html_route(client: TestClient) -> None:
+    test_path = "/test_registered_html"
+    mock_file_contents = '<h1 class="test">This is test file content</h1>'
+
+    # Test that we can register a HTML page
+    register_html_page(test_path,title="my mock html page", html_file="test.txt",show_in_navbar=True)
+
+    # And that it gets added correctly as a route in the app
+    app.include_router(get_custom_router())
+    assert test_path in _CUSTOM_ROUTES
+
+    # confirm that the route also leads to the file being returned successfully.
+    m = MagicMock(read_text=MagicMock(return_value=mock_file_contents))
+    _CUSTOM_ROUTES[test_path].html_file = m
+
+    response = client.get(test_path)
+    assert response.status_code == 200
+    assert mock_file_contents in response.text
+
