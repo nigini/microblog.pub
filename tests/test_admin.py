ef3bee8bdb113000b81df03c93232a1b7df50db1
1dom <>
2024-10-10 22:57:26 +0100

    Add admin homepage test

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -3,7 +3,12 @@ import typing
 import starlette
 from fastapi.testclient import TestClient
 
+from app.config import generate_csrf_token
 from app.main import app
+from tests.utils import generate_admin_session_cookies
+
+from app import activitypub as ap
+
 
 
 def test_admin_endpoints_are_authenticated(client: TestClient) -> None:
@@ -25,3 +30,20 @@ def test_admin_endpoints_are_authenticated(client: TestClient) -> None:
             routes_tested.append((method, route.path))
 
     assert len(routes_tested) > 0
+
+
+def test_public_works_authenticated(client: TestClient) -> None:
+    response = client.post(
+        "/admin/actions/new",
+        data={
+            "content": "hello",
+            "redirect_url": "http://testserver/",
+            "visibility": ap.VisibilityEnum.PUBLIC.name,
+            "csrf_token": generate_csrf_token(),
+        },
+        cookies=generate_admin_session_cookies(),
+        follow_redirects=False,
+    )
+    assert response.status_code == 302
+    resp = client.get("/",cookies=generate_admin_session_cookies())
+    assert resp.status_code == 200
